326bc41b3d88de237de0e74a4d7a2a831463ef66
willing nguyen <worldforce2011@gmail.com>
2011-10-17 10:28:25 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NodeLinkTree::Application.config.secret_token = 'd9ca54eb7cdb86aa4c4f57a2681183cbe736cc6f210e89978ffd7042f312e74dbb8bb721c7584b41fef7186a3b75622188062e7c06111b1c06956839cfb8a1cb'
